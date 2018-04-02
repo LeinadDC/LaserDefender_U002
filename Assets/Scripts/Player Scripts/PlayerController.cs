bb46eddb360f2708b6fc57c90883ece206d9dff3
Daniel Castellano <daniel.castellanom@gmail.com>
2018-04-02 17:47:21 -0600

    First Commit

---
+++ Assets/Scripts/Player Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public int shipSpeed;
+    private Rigidbody2D rigidbody;
+
+	void Start () {
+        rigidbody = GetComponent<Rigidbody2D>();
+	}
+	
+	
+	void Update () {
+        float inputAxis = Input.GetAxis("Horizontal");
+        Vector2 movement = new Vector2(inputAxis, 0);
+        rigidbody.AddForce(movement * shipSpeed * Time.deltaTime);
+	}
+}
